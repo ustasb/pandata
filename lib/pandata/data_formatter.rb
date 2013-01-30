5e822b5a2eed3ca1efe7568c965d0802fe7ada79
Brian Ustas <brianustas@gmail.com>
2013-01-30 05:21:10 -0500

    Ignore initial variants of 'the' when sorting strings

--- lib/pandata/data_formatter.rb
+++ lib/pandata/data_formatter.rb
@@ -10,7 +10,7 @@ module Pandata
     end
 
     def uniq_sort_list values
-      list values.uniq.sort
+      list custom_sort(values.uniq)
     end
 
     def tracks tracks
@@ -23,15 +23,23 @@ module Pandata
 
     def followx value
       str = ''
+
       value.each do |hash|
         str << "  - name: #{hash[:name]}\n"
         str << "    webname: #{hash[:webname]}\n"
         str << "    href: #{hash[:href]}\n"
       end
+
       str
     end
 
     private
+
+    def custom_sort(enum)
+      # Ignore the initial 'The' when sorting strings.
+      # Useful for sorting artist/ song titles.
+      enum.sort_by { |string| string.sub(/^the\s*/i, '') }
+    end
 
     def artists_items value, item_name
       artist_items = {}
@@ -41,8 +49,10 @@ module Pandata
         (artist_items[artist_name] ||= Set.new) << hash[item_name]
       end
 
+      artist_items = custom_sort(artist_items)
+
       str = ''
-      artist_items.sort.each do |artist_name, items|
+      artist_items.each do |artist_name, items|
         str << "  - #{artist_name}\n"
         items.sort.each do |item|
           str << "      - #{item}\n"
